3e0bb589831e358efcfcab53fed6dd2260c36f6c
Christopher Olson <cjolson20@gmail.com>
2016-01-13 00:19:48 -0500

    Create BackwardAndForward.py

---
+++ BackwardAndForward.py
@@ -0,0 +1,22 @@
+"""
+The sign outside reads: Name no one man.
+
+"Escape. We must escape." Staring at the locked door of his cage, Beta Rabbit, spy and brilliant mathematician, 
+has a revelation. "Of course! Name no one man - it's a palindrome! Palindromes are the key to opening this lock!"
+
+To help Beta Rabbit crack the lock, write a function answer(n) which returns the smallest positive integer base b, 
+at least 2, in which the integer n is a palindrome. The input n will satisfy "0 <= n <= 1000."
+"""
+import string
+def frm(x, b):
+    r = ''
+    while x > 0:
+        r = string.printable[x % b] + r
+        x //= b
+    return r
+
+def answer(n):
+    for i in range(2, 37):
+        pali = frm(n, i)
+        if ''.join(reversed(pali)) == pali:
+            return i
