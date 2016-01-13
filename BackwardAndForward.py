4065c48262b765a8bf1ee7ae921505634628a01c
Christopher Olson <cjolson20@gmail.com>
2016-01-13 12:23:35 -0500

    cleaned the conv2base function

--- BackwardAndForward.py
+++ BackwardAndForward.py
@@ -7,16 +7,16 @@ has a revelation. "Of course! Name no one man - it's a palindrome! Palindromes a
 To help Beta Rabbit crack the lock, write a function answer(n) which returns the smallest positive integer base b, 
 at least 2, in which the integer n is a palindrome. The input n will satisfy "0 <= n <= 1000."
 """
-import string
-def frm(x, b):
+def conv2base(x, b):
+#converts any number x into any base b
     r = ''
     while x > 0:
-        r = string.printable[x % b] + r
+        r += str(x%b)
         x //= b
     return r
 
 def answer(n):
     for i in range(2, 37):
-        pali = frm(n, i)
+        pali = conv2base(n, i)
         if ''.join(reversed(pali)) == pali:
             return i
