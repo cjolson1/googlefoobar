e8fb20acbc6e93ecf9fedc1bed94f1c2c781ca7d
Christopher Olson <cjolson20@gmail.com>
2016-01-13 12:42:57 -0500

    added missing instructions

--- APirateWalksIntoABar.py
+++ APirateWalksIntoABar.py
@@ -1,3 +1,8 @@
+"""
+can't find the instructions for this anywhere! but essentially this problem asks for you to find the amount of pirates that form a loop
+when asking about a topic because they keep referencing you to other pirates. the input is a list where the indices correspond
+to number of the pirate and the value at the indice corresponds to which pirate they reference you to,
+"""
 def answer(numbers):
     for n in range(len(numbers)):
         reference = numbers[n]
