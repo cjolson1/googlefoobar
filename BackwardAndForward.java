01e3ed33dde937b0cda8cfe71dea4cecca03e02d
Christopher Olson <cjolson20@gmail.com>
2016-01-16 14:14:10 -0500

    Update BackwardAndForward.java

--- BackwardAndForward.java
+++ BackwardAndForward.java
@@ -1,3 +1,11 @@
+/*
+The sign outside reads: Name no one man.
+"Escape. We must escape." Staring at the locked door of his cage, Beta Rabbit, spy and brilliant mathematician, 
+has a revelation. "Of course! Name no one man - it's a palindrome! Palindromes are the key to opening this lock!"
+To help Beta Rabbit crack the lock, write a function answer(n) which returns the smallest positive integer base b, 
+at least 2, in which the integer n is a palindrome. The input n will satisfy "0 <= n <= 1000."
+*/
+
 public class BackwardAndForward {
     public static int answer(int n){
         int ans = 0;
