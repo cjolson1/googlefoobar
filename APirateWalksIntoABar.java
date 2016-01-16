846a400afe673cfb64b9d72e4c38aed843b10d8d
Christopher Olson <cjolson20@gmail.com>
2016-01-16 14:13:38 -0500

    updated instructions

--- APirateWalksIntoABar.java
+++ APirateWalksIntoABar.java
@@ -1,3 +1,10 @@
+/*
+
+can't find the instructions for this anywhere! but essentially this problem asks for you to find the amount of pirates that form a loop
+when asking about a topic because they keep referencing you to other pirates. the input is a list where the indices correspond
+to number of the pirate and the value at the indice corresponds to which pirate they reference you to,
+
+*/
 public class Answer {
     public static int answer(int[] numbers){
         int ans = 0;
